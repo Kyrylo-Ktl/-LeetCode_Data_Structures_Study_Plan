988fc43a3a1d415bbe7ddff8cac63d5f7f6c3f36
Kyrylo-Ktl <kyrylo.ktl@gmail.com>
2022-07-06 16:40:10 +0300

    DS-I 5th day done

---
+++ Data Structure I /Day 5 Array/Search a 2D Matrix.py
@@ -0,0 +1,36 @@
+from typing import List
+
+
+class Solution:
+    """
+    Time:   O(n*m)
+    Memory: O(1)
+    """
+
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        return any(target in row for row in matrix)
+
+
+class Solution:
+    """
+    Time:   O(log(n*m))
+    Memory: O(1)
+    """
+
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        n, m = len(matrix), len(matrix[0])
+        left, right = 0, n * m - 1
+
+        while left <= right:
+            mid = (left + right) // 2
+            num = matrix[mid // m][mid % m]
+
+            if num == target:
+                return True
+
+            if num < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+
+        return False
---
+++ Data Structure I /Day 5 Array/Valid Sudoku.py
@@ -0,0 +1,52 @@
+from typing import List
+
+
+class Solution:
+    """
+    Time:   O(n^3)
+    Memory: O(1)
+    """
+
+    SIZE = 3
+    EMPTY = '.'
+
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        for i in range(self.SIZE ** 2):
+            for j in range(self.SIZE ** 2):
+                if board[i][j] == self.EMPTY:
+                    continue
+
+                for k in range(self.SIZE ** 2):
+                    if board[i][k] == board[i][j] and k != j:
+                        return False
+                    if board[k][j] == board[i][j] and k != i:
+                        return False
+
+                block_row = i - i % self.SIZE
+                block_col = j - j % self.SIZE
+                for r in range(block_row, block_row + self.SIZE):
+                    for c in range(block_col, block_col + self.SIZE):
+                        if board[r][c] == board[i][j] and (r != i or c != j):
+                            return False
+
+        return True
+
+
+class Solution:
+    """
+    Time:   O(n^2)
+    Memory: O(n^2)
+    """
+
+    SIZE = 3
+    EMPTY = '.'
+
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        seen = (
+            [(val, r), (c, val), (r // self.SIZE, c // self.SIZE, val)]
+            for r, row in enumerate(board)
+            for c, val in enumerate(row)
+            if val != self.EMPTY
+        )
+        seen = sum(seen, [])
+        return len(seen) == len(set(seen))
